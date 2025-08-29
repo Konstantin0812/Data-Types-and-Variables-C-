8ae06117da433d10d8ea3f4547d508880a64cd14
Konstantin0812 <titi081203@gmail.com>
2025-08-29 16:03:26 +0300

    Add files via upload
    Add C# code

--- Program.cs
+++ Program.cs
@@ -1,14 +1,26 @@
-﻿namespace _06._Reversed_Chars
+﻿namespace _12._Refactor_Special_Numbers
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string firstString = Console.ReadLine();
-            string secondString = Console.ReadLine();
-            string thirdString = Console.ReadLine();
-
-           Console.WriteLine($"{thirdString} {secondString} {firstString}");
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int digit = 0;
+            bool isSpecialNum = false;
+            for(int i = 1; i <= number; i++)
+            {
+                digit = i;
+                while (i > 0)
+                {
+                    sum += i % 10;
+                    i /= 10;
+                }
+                isSpecialNum = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine("{0} -> {1}", digit, isSpecialNum);
+                sum = 0;
+                i = digit;
+            }
         }
     }
 }
